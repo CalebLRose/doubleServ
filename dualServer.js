79c58d73e5b6466e43bf649cd99d0e406b0d57da
Caleb Rose <clrose86@gmail.com>
2018-02-03 10:40:26 -0600

    servers connecting

--- dualServer.js
+++ dualServer.js
@@ -6,9 +6,27 @@ var PORT1 = 7000;
 // bad port
 var PORT2 = 7500;
 
+// this will give positive reinforcement
 function positive(request, response){
 	response.end("This is the good one")
 };
 
-var niceServer = http
+var niceServer = http.createServer(positive);
+
+niceServer.listen(PORT1, function(){
+	console.log("connected to happy port:"+PORT1)
+});
+
+
+// this will give negative reinforcement
+
+function negative(request, response){
+	response.end("This is the bad one")
+};
+
+var meanServer = http.createServer(negative);
+
+meanServer.listen(PORT2, function(){
+	console.log("connected to unhappy port:"+PORT2)
+});
 
